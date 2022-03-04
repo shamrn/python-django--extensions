82fdd1a2d9ddfe48ab28070c3d67d04e887dfff2
Vladislav Shamrin <v.shamrin@spider.ru>
2022-03-04 11:56:37 +0300

    Update django-trigram_similarity

--- django-trigram-similarity/extension.py
+++ django-trigram-similarity/extension.py
@@ -2,7 +2,7 @@ from django.db.models.functions import Greatest
 from django.contrib.postgres.search import TrigramSimilarity
 
 
-def greatest_trigram_similarity(fields: list, value: str):
+def greatest_trigram_similarity(fields: list, value: str) -> Greatest:
     """
     It takes as input a list of fields to search for "['name', 'title', ....]",
     and the search "value", and returns the result for the most relevant field.

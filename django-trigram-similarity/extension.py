35c24b162a9fc96aa1d00fe868de92fc0789c80b
Vladislav Shamrin <shamrin2007@mail.ru>
2022-12-01 14:18:16 +0300

    Added word similarity in django-trigram-similarity

--- django-trigram-similarity/extension.py
+++ django-trigram-similarity/extension.py
@@ -9,3 +9,17 @@ def greatest_trigram_similarity(fields: list, value: str) -> Greatest:
     """
 
     return Greatest(*[TrigramSimilarity(field, value) for field in fields])
+
+
+# For django 4
+
+from django.contrib.postgres.search import TrigramWordSimilarity
+
+
+def greatest_trigram_similarity(fields: list, value: str) -> Greatest:
+    """
+    It takes as input a list of fields to search for "['name', 'title', ...]",
+    and the search "value", and returns the result for the most relevant field.
+    """
+
+    return Greatest(*[TrigramWordSimilarity(value, field) for field in fields])

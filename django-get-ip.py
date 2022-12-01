48f2e78413c72c86a53ea4349e7986a0a96fdb08
Vladislav Shamrin <shamrin2007@mail.ru>
2022-12-01 20:23:04 +0300

    Refactoring 'django-get-ip' extension

--- django-get-ip.py
+++ django-get-ip.py
@@ -1,5 +1,6 @@
 def get_request_ip(request) -> Union[str, None]:
     """Get user ip."""
+
     meta_dict = request.META
     x_forwarded_for = meta_dict.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
